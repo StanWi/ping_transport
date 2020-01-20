1076bab1f6f1c430fe6c06a4cab2e194d4ce9202
Stan <26521410+StanWi@users.noreply.github.com>
2020-01-20 11:34:24 +0800

    ip list in yaml

--- ping_transport.py
+++ ping_transport.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
-
 import os
-from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 
-file_name = 'transport_ip_list.txt'
-addresses = []
-down_list = []
+import yaml
 
-def addresses_list(file_name):
-    """Return list of list IP addresses and hostnames"""
-    with open(file_name) as ip_list:
-        for string in ip_list:
-            if not string.startswith('#'):
-                addresses.append(string.strip().split(';'))
 
-def check_ping(ip: str, name: str) -> None:
-    """Check ping IP, if not, add IP to list."""
-    response = os.system('timeout 1 ping -c 1 -i 1 ' + ip + ' > /dev/null')
+def load_addresses(file: str) -> dict:
+    """Return dict ip addresses from settings"""
+    try:
+        with open(file) as f:
+            addresses = yaml.safe_load(f)
+    except FileNotFoundError:
+        addresses = {'addresses': {'127.0.0.1': 'localhost'}}
+    return addresses['addresses']
+
+
+def check_ping(address: str, name: str) -> None:
+    """Check ping IP, if not, add IP to list"""
+    response = os.system('timeout 1 ping -c 1 -i 1 ' + address + ' > /dev/null')
     if response != 0:
-        down_list.append(name + ' (' + ip + ') is down!')
-
-def  threads_conn(function, hostname, limit=2):
-    with ThreadPoolExecutor(max_workers=limit) as executor:
-        pass
+        print('{} ({}) is down!'.format(name, address))
 
 
 if __name__ == '__main__':
     start_time = datetime.now()
-    addresses_list(file_name)
-    for hostname in addresses:
-        check_ping(hostname[0], hostname[1])
-    for line in down_list:
-        print(line)
-    print(len(down_list))
+    down_list = []
+    file_addresses = 'transport_ip_list.yml'
+    ip = load_addresses(file_addresses)
+    for ip_address in ip.keys():
+        check_ping(ip_address, ip[ip_address])
     print(datetime.now() - start_time)
